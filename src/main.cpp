6728db53dd6829038a07a681b0bb6eb36e0d8d21
Bethl <bethl@local>
2025-11-25 15:04:46 -0800

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@
 PCA9685 pca(B000000, Wire, 400000);
 
 // Drive instance: (pot pin, low offset, high offset, pot angle, pca instance, channel, min limit, max limit)
-Drive base_rotation      (A0, 0, 1023, 270, &pca, 0, 0, 270);
-Drive elbow_drive        (A1, 0, 1023, 270, &pca, 1, 0, 270);
-Drive wrist_drive        (A2, 0, 1023, 270, &pca, 2, 0, 270);
-Drive End_effector_drive (A3, 0, 1023, 270, &pca, 3, 0, 270);
+Drive base_rotation   (A0, 0, 1023, 270, &pca, 0, 0, 270);
+Drive shoulder_drive  (A1, 0, 1023, 270, &pca, 1, 0, 180);
+Drive elbow_drive     (A2, 0, 1023, 270, &pca, 2, 0, 200);
+Drive wrist_drive     (A3, 0, 1023, 270, &pca, 3, 0, 270);
 
 void setup() {
   Serial.begin(115200);
@@ -46,5 +46,5 @@ void loop() {
   
   Serial.print(" Base rotation angle: ");
   Serial.println(base_rotation.servo.getAngle());
-  delay(50);
+  delay(200);
 }
